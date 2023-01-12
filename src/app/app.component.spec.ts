4fe7cc8973d7ff634827bc34a5de092f2bd545a1
IagoPL <iagoprietolamas@gmail.com>
2023-01-12 18:44:13 +0100

    init catmania

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'catmania'`, () => {
+  it(`should have as title 'CatMania'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catmania');
+    expect(app.title).toEqual('CatMania');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('catmania app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CatMania app is running!');
   });
 });
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,11 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 //TODO: Definir las ruta/s que correspondan
 const routes: Routes = [
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class PagesRoutingModule { }
